01d740a311f963fb83ba38d924c44f4448ad7471
ligiadavilabozzi <ligia.bozzi@gmail.com>
2021-05-14 20:14:25 -0300

    Correct errors

--- src/componentes/HomePage/index.js
+++ src/componentes/HomePage/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Card from '../Card/'
-import '.style.css'
+import './style.css'
 export default function HomePage(){
     return(
         <div className="home-page">
