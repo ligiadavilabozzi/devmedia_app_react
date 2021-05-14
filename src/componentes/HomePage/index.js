91236583874af31437fb543b770abddc7d02f00a
ligiadavilabozzi <ligia.bozzi@gmail.com>
2021-05-14 20:09:36 -0300

    HomePage Style

--- src/componentes/HomePage/index.js
+++ src/componentes/HomePage/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Card from '../Card/'
-
+import '.style.css'
 export default function HomePage(){
     return(
-        <div>
+        <div className="home-page">
             <Card />
             <Card />
             <Card />
