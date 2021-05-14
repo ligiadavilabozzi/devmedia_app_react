a79ab1d30602fa6418af001ee193766b0834ab60
ligiadavilabozzi <ligia.bozzi@gmail.com>
2021-05-14 20:08:08 -0300

    Card Style

--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import './style.css'
 export default function Card(){
     return(
-        <div>
+        <div className="card-container">
         <p>Componentes:</p>
         <p>Facilita em manter partes menores funcionando corretamente</p>
         <p>Você pode reutilizá-los, ou seja, menos código para se escrever</p>
